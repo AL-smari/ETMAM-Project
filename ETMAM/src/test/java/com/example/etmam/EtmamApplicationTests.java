3d4fcbdff4fca67abe229b2fcfc338f5c2af81cc
abdulaziz alsmari <148686097+AL-smari@users.noreply.github.com>
2024-01-04 10:40:09 +0300

    Add files via upload

---
+++ ETMAM/src/test/java/com/example/etmam/EtmamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etmam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtmamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
